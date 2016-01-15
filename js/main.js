2dc7fbe47a8b984a760e7387d5574d3ca2bbea68
hmasiello <hmasiello@transcen.org>
2016-01-14 21:15:38 -0500

    Finish form for Contact Us page

--- js/main.js
+++ js/main.js
@@ -1,27 +1,72 @@
 //$.ready makes sure DOM is loaded before anything happens 
 $( document ).ready(function(){
 
+
 	//event prevent default 
-	$('.form').submit(function(event) {
-	  event.preventDefault();
-	});
+	$('#theform').submit(function(e) {
+	  e.preventDefault();
+	
+	//CONTACT US PAGE 
+	//validate & answer to antispam 
 
-	//validate 
-	jQuery.validator.setDefaults({
-  	debug: true,
-  	success: "valid"
-	});
-	$('#theform').validate({
-		rules: {
-			'name': {
-				required: true,
-				minlength: 4
-			}
+		//name 
+		var nameElement = $('#name');
+		var nameValue = nameElement.val();
+
+		if ( nameValue == '') {
+			alert('Please enter a name');
+			return; 
 		}
-	})
 
-	//answer to #antispam
-	$
+		//email
+		var emailElement = $('#email');
+		var emailValue = emailElement.val();
+
+		if ( emailValue == '') {
+			alert ('Please enter an email address');
+			return; 
+		}
+
+		//message 
+		var messageElement = $('#msg');
+		var messageValue = messageElement.val()
+
+		if (messageValue == '') {
+			alert ('Please enter a message');
+			return;
+		}
+
+		//antispam 
+		var antispamElement = $('#antispam');
+		var antispamValue = antispamElement.val()
+
+		if (antispamValue != '4'){ 
+			alert ('Try again!');
+			return;
+		}
+	
+		//successful submit 
+		alert ('Thanks for submitting the form, someone will be in touch with you soon!')
+
+
+	//SUCCESS STORIES 
+	//slider show more show less
+
+		var readMore = $('.readmore a')
+		var paragraphHide = $('p.hide')
+		var readLess = $('.readless')
+
+		$('readMore').click(function() {
+			$('paragraphHide').slideDown();
+			$('readMore').hide();
+		});
+
+		$('readLess').click(function() {
+			$('paragraphHide').slideUp();
+			$('readMore').show();	
+		})
+
+	});
 
 })
 
